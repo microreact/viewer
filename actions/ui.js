cf34da837dbd6f10999c2792666a0c1c17c0ca29
Khalil <ka10@sanger.ac.uk>
2021-10-30 15:38:38 +0100

    Update ui.js

--- actions/ui.js
+++ actions/ui.js
@@ -101,9 +101,9 @@ export function addFiles(rawFiles, paneId) {
 
 export function addHistoryEntry(source, label) {
   return {
-    type: "MICROREACT VIEWER/ADD HISTORY ENTRY",
     label: `${source}: ${label}`,
     payload: undefined,
+    type: "MICROREACT VIEWER/ADD HISTORY ENTRY",
   };
 }
 
