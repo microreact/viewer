270908c63a053f1b11bd7b7ad771f057a7415a56
Khalil <ka10@sanger.ac.uk>
2022-01-12 14:07:31 +0000

    https://gitlab.com/cgps/microreact/support/-/issues/506

--- selectors/styles/colours-legend-entries.js
+++ selectors/styles/colours-legend-entries.js
@@ -3,6 +3,8 @@ import { sortComparator } from "../../utils/arrays";
 import { toText } from "../../utils/text";
 
 import dataColumnByFieldSelector from "../datasets/data-column-by-field";
+import uniqueValuesSelector from "../datasets/unique-values";
+import activeRowsWithStyleFieldsSelector from "../filters/active-rows-with-style-fields";
 import selectedRowsSelector from "../filters/selected-rows";
 import colourMapForFieldSelector from "./colour-map-for-field";
 
@@ -15,19 +17,81 @@ function isSelected(rows, field, value) {
   return false;
 }
 
-const coloursLegendEntriesSelector = createSelector(
-  (state, field) => dataColumnByFieldSelector(state, field),
-  (state, field) => colourMapForFieldSelector(state, field),
-  (state) => selectedRowsSelector(state),
-  (
-    dataColumn,
-    colourMap,
-    selectedRows,
-  ) => {
-    const entries = [];
+// const coloursLegendEntriesSelector = createSelector(
+//   (state) => activeRowsWithStyleFieldsSelector(state),
+//   (state, field) => dataColumnByFieldSelector(state, field),
+//   (state, field) => colourMapForFieldSelector(state, field),
+//   (state) => selectedRowsSelector(state),
+//   (
+//     { rows },
+//     dataColumn,
+//     colourMap,
+//     selectedRows,
+//   ) => {
+//     const entries = [];
 
-    if (colourMap.scale === "discrete") {
-      for (const [ value, colour ] of colourMap.entries()) {
+//     if (colourMap.scale === "discrete") {
+//       for (const [ value, colour ] of colourMap.entries()) {
+//         entries.push({
+//           colour,
+//           value,
+//           label: toText(
+//             dataColumn.dataType,
+//             value,
+//           ),
+//           isSelected: (selectedRows) && isSelected(selectedRows, dataColumn.name, value),
+//         });
+//       }
+
+//       // order the list of groups by label
+//       entries.sort(sortComparator("label"));
+//     }
+
+//     else if (colourMap.scale === "binned") {
+//       for (let index = 1; index <= colourMap.numberOfBins; index++) {
+//         const start = colourMap.domain[0] + colourMap.binLength * (index - 1);
+//         const end = colourMap.domain[0] + colourMap.binLength * index;
+//         entries.push({
+//           colour: colourMap.colourGetter(index),
+//           value: [ start, end ],
+//           label: `${toText(dataColumn.dataType, start.toFixed(2))} - ${toText(dataColumn.dataType, end.toFixed(2))}`,
+//         });
+//       }
+//     }
+
+//     else if (colourMap.scale === "continuous") {
+//       entries.push({
+//         colour: colourMap.range[0],
+//         value: colourMap.domain[0],
+//         label: toText(dataColumn.dataType, colourMap.domain[0]),
+//       });
+//       entries.push({
+//         colour: colourMap.range[1],
+//         value: colourMap.domain[1],
+//         label: toText(dataColumn.dataType, colourMap.domain[1]),
+//       });
+//     }
+
+//     return {
+//       scale: colourMap.scale,
+//       entries,
+//     };
+//   }
+// );
+
+function coloursLegendEntriesSelector(state, field) {
+  const colourMap = colourMapForFieldSelector(state, field);
+  const dataColumn = dataColumnByFieldSelector(state, field);
+  const entries = [];
+
+  if (colourMap.scale === "discrete") {
+    const uniqueValues = new Set();
+    const selectedRows = selectedRowsSelector(state, field);
+    const { rows } = activeRowsWithStyleFieldsSelector(state);
+    for (const row of rows) {
+      const value = row[field];
+      if (!uniqueValues.has(value)) {
+        const colour = colourMap.get(value);
         entries.push({
           colour,
           value,
@@ -37,42 +101,16 @@ const coloursLegendEntriesSelector = createSelector(
           ),
           isSelected: (selectedRows) && isSelected(selectedRows, dataColumn.name, value),
         });
-      }
-
-      // order the list of groups by label
-      entries.sort(sortComparator("label"));
-    }
-
-    else if (colourMap.scale === "binned") {
-      for (let index = 1; index <= colourMap.numberOfBins; index++) {
-        const start = colourMap.domain[0] + colourMap.binLength * (index - 1);
-        const end = colourMap.domain[0] + colourMap.binLength * index;
-        entries.push({
-          colour: colourMap.colourGetter(index),
-          value: [ start, end ],
-          label: `${toText(dataColumn.dataType, start.toFixed(2))} - ${toText(dataColumn.dataType, end.toFixed(2))}`,
-        });
+        uniqueValues.add(value);
       }
     }
-
-    else if (colourMap.scale === "continuous") {
-      entries.push({
-        colour: colourMap.range[0],
-        value: colourMap.domain[0],
-        label: toText(dataColumn.dataType, colourMap.domain[0]),
-      });
-      entries.push({
-        colour: colourMap.range[1],
-        value: colourMap.domain[1],
-        label: toText(dataColumn.dataType, colourMap.domain[1]),
-      });
-    }
-
-    return {
-      scale: colourMap.scale,
-      entries,
-    };
+    entries.sort(sortComparator("label"));
   }
-);
+
+  return {
+    scale: colourMap.scale,
+    entries,
+  };
+}
 
 export default coloursLegendEntriesSelector;
