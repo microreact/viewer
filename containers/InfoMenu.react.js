b34bf436973f1270478bdb0859c60b307207d275
Khalil <ka10@sanger.ac.uk>
2022-03-02 14:44:00 +0000

    Update InfoMenu.react.js

--- containers/InfoMenu.react.js
+++ containers/InfoMenu.react.js
@@ -1,11 +1,8 @@
 import Component from "../components/InfoMenu.react";
 import { connectToPresentStateWithRef } from "../utils/state";
 
-const mapStateToProps = (state) => {
+function mapStateToProps(state) {
   return state.meta;
-};
+}
 
-const mapDispatchToProps = (dispatch) => ({
-});
-
-export default connectToPresentStateWithRef(Component, mapStateToProps, mapDispatchToProps);
+export default connectToPresentStateWithRef(Component, mapStateToProps);
