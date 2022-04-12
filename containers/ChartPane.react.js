f0adc536daef54fe0996e270a784f8f080c12298
Khalil <ka10@sanger.ac.uk>
2022-04-12 12:46:35 +0100

    Update ChartPane.react.js

--- containers/ChartPane.react.js
+++ containers/ChartPane.react.js
@@ -6,12 +6,14 @@ import { selectItem } from "../actions/charts";
 import chartDataSelector from "../selectors/charts/chart-data";
 import vegaSpecSelector from "../selectors/charts/vega-spec";
 import chartTypeSelector from "../selectors/charts/chart-type";
+import seriesFieldSelector from "../selectors/charts/series-field";
 
 const mapStateToProps = (state, { chartId }) => {
   return {
     chartData: chartDataSelector(state),
     chartType: chartTypeSelector(state, chartId),
     vegaSpec: vegaSpecSelector(state, chartId),
+    seriesDataColumn: seriesFieldSelector(state, chartId),
   };
 };
 
