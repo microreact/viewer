7ae01ce1cc4439d3aff47869dea41ba1011be6d5
Khalil <ka10@sanger.ac.uk>
2022-05-05 12:45:37 +0100

    Update DownloadFilesMenuContent.react.js

--- components/DownloadFilesMenuContent.react.js
+++ components/DownloadFilesMenuContent.react.js
@@ -15,6 +15,15 @@ const icons = {
 
 class DownloadFilesMenuContent extends React.PureComponent {
 
+  static normmaliseUrl(url) {
+    if (typeof url === "string") {
+      return url.replace("https://beta.microreact.org/", "https://microreact.org/");
+    }
+    else {
+      return url;
+    }
+  }
+
   filesSelector() {
     const { props } = this;
     return Object.values(props.files);
@@ -23,22 +32,25 @@ class DownloadFilesMenuContent extends React.PureComponent {
   render() {
     return (
       this.filesSelector().map(
-        (item) => (
-          <UiDropdownMenu.Item
-            key={item.id}
-            alignItems="flex-start"
-            button
-            component={item.url ? "a" : undefined}
-            href={item.url ?? undefined}
-            target={item.url ? "_blank" : undefined}
-            download={item.url ? item.name : undefined}
-            onClick={item.blob ? () => downloadDataUrl(item.blob, item.name, item.format) : undefined}
-          >
-            <PaneIcon component={icons[item.type]} />
-            &emsp;
-            { item.name || `${item.type} file` }
-          </UiDropdownMenu.Item>
-        )
+        (item) => {
+          const normmalisedUrl = this.normmaliseUrl(item.url);
+          return (
+            <UiDropdownMenu.Item
+              key={item.id}
+              alignItems="flex-start"
+              button
+              component={normmalisedUrl ? "a" : undefined}
+              href={normmalisedUrl ?? undefined}
+              target={normmalisedUrl ? "_blank" : undefined}
+              download={normmalisedUrl ? item.name : undefined}
+              onClick={item.blob ? () => downloadDataUrl(item.blob, item.name, item.format) : undefined}
+            >
+              <PaneIcon component={icons[item.type]} />
+              &emsp;
+              { item.name || `${item.type} file` }
+            </UiDropdownMenu.Item>
+          );
+        }
       )
     );
   }
