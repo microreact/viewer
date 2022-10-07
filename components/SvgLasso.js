b575d8a395d517cc7d98457b03210ab4b63d2577
Khalil <khalil.abudahab@cgps.group>
2022-10-07 17:38:24 +0100

    Update SvgLasso.js

--- components/SvgLasso.js
+++ components/SvgLasso.js
@@ -1,6 +1,6 @@
-import React from "react";
-import PropTypes from "prop-types";
 import Draggable from "react-draggable";
+import PropTypes from "prop-types";
+import React from "react";
 
 function isClosedPath(newPath) {
   return (
