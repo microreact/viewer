d877f1180a2dabbdb355e2e958e516fb34654d91
Khalil <khalil.abudahab@cgps.group>
2022-10-19 20:15:05 +0100

    Update TitleEditor.react.js

--- components/TitleEditor.react.js
+++ components/TitleEditor.react.js
@@ -2,41 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import { DebounceInput } from "react-debounce-input";
 
-const KeyCode = {
-  "ESC": 27,
-  "ENTER": 13,
-};
 import styles from "../styles/title-editor.module.css";
 
 function TitleEditor(props) {
-  const defaultTitle = props.meta.name;
-  const inputRef = React.useRef();
-  const [text, setText] = React.useState(defaultTitle);
-
-  function onKeyDown(e) {
-    // on pressing enter key the text gets updated in the redux-state
-    if (e.keyCode === KeyCode.ENTER) {
-      inputRef.current.blur();
-      props.onSaveText(text);
-    }
-
-    // on pressing esc updates to the previous stored redux state and default mode.
-    if (e.keyCode === KeyCode.ESC) {
-      inputRef.current.blur();
-      setText(defaultTitle);
-    }
-  }
-
   return (
-     <DebounceInput
+    <DebounceInput
       className={styles.editTextInput}
-      onKeyDown={onKeyDown}
-      inputRef={inputRef}
-      onChange={(e) => setText(e.target.value)}
-      dir={"ltr"}
-      />
       value={props.value}
+      debounceTimeout={1000}
       onChange={(e) => props.onChange(e.target.value)}
+    />
   );
 }
 
