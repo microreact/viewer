b151319792f666dca0db341073a01f98509a2052
Khalil <khalil.abudahab@cgps.group>
2023-05-19 20:19:37 +0100

    TASK-658

--- utils/loaders/xlsx.js
+++ utils/loaders/xlsx.js
@@ -8,13 +8,15 @@ import { createBasicDataset } from "../datasets";
 
 function readSpeadsheetFile(arrayBuffer, options) {
   const workbook = options.XLSX.read(
-    arrayBuffer, // new Uint8Array(arrayBuffer),
-    { type: "array" },
+    arrayBuffer,
   );
 
   const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 
-  const result = options.XLSX.utils.sheet_to_json(worksheet);
+  const result = options.XLSX.utils.sheet_to_json(
+    worksheet,
+    { header: 1 },
+  );
 
   return result;
 }
@@ -39,5 +41,23 @@ export async function loadSpeadsheetFile(input) {
       XLSX,
     },
   );
-  return createBasicDataset(data);
+
+  const rows = [];
+  const headers = data[0];
+  for (let rowIndex = 1; rowIndex < data.length; rowIndex++) {
+    const rawRow = data[rowIndex];
+    const row = {
+      "--mr-index": rows.length.toString(),
+    };
+    for (let colIndex = 0; colIndex < headers.length; colIndex++) {
+      const header = headers[colIndex];
+      row[header] = rawRow[colIndex];
+    }
+    rows.push(row);
+  }
+
+  return createBasicDataset(
+    rows,
+    headers,
+  );
 }
