92eecd99913f340c110bfb19b960a2b25449ed77
James Baker <5226213+JayboBaker@users.noreply.github.com>
2023-07-04 14:26:55 +0100

    TASK-728 - Codetidy

--- components/TableComponents.react.js
+++ components/TableComponents.react.js
@@ -1,39 +1,10 @@
 /* eslint "react/prop-types": 0 */
 
 import React from "react";
-import { sortableContainer, sortableElement, sortableHandle } from "react-sortable-hoc";
+import { sortableHandle } from "react-sortable-hoc";
 import DragIndicatorOutlinedIcon from "@mui/icons-material/DragIndicatorOutlined";
-import CheckBoxTwoToneIcon from "@mui/icons-material/CheckBoxTwoTone";
-import CheckBoxOutlineBlankTwoToneIcon from "@mui/icons-material/CheckBoxOutlineBlankTwoTone";
 
-// import "../styles/table.css";
 import TableHeaderMenu from "../containers/TableColumnMenu.react";
-
-const cursorStyle = { cursor: "pointer" };
-
-const SortableItem = sortableElement(
-  ({ children }) => children
-);
-
-export const SortableContainer = sortableContainer(
-  ({ children }) => {
-    return (
-      <div
-        style={
-          {
-            display: "flex",
-            width: "100%",
-            height: "100%",
-            // "white-space": "nowrap",
-            alignItems: "center",
-          }
-        }
-      >
-        {children}
-      </div>
-    );
-  }
-);
 
 const DraggableHandle = sortableHandle(
   () => (
@@ -48,42 +19,7 @@ const DraggableHandle = sortableHandle(
   )
 );
 
-// export const HeaderCellComponent = (props) => {
-//   console.log("HeaderCellComponent props: ", { props })
-
-//   const { index, children, ...rest } = props;
-//   return (
-//     <SortableItem key={`item-${index}`} index={index}>
-//       <div
-//         {...rest}
-//         onClick={
-//           (event) => {
-//             if (
-//               event.target
-//               &&
-//               event.target.classList
-//               &&
-//               (
-//                 event.target.classList.contains("BaseTable__header-cell")
-//                 ||
-//                 event.target.classList.contains("BaseTable__header-cell-text")
-//                 ||
-//                 event.target.classList.contains("BaseTable__sort-indicator")
-//               )
-//             ) {
-//               rest.onClick(event);
-//             }
-//           }
-//         }
-//       >
-//         {children}
-//       </div>
-//     </SortableItem>
-//   );
-// };
-
 export function HeaderTextComponent(props) {
-  console.log("HeaderTextComponent props: ", { props })
   if (props.column.colDef.dataKey === "--microreact-selection-cell") {
     return null;
   }
