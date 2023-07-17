9a7f0c0a67e8db195e550654935f0a778d6719a6
Khalil <khalil.abudahab@cgps.group>
2023-07-17 20:29:18 +0100

    Move background colour to css.

--- selectors/timelines/filtered-range-chart-spec.js
+++ selectors/timelines/filtered-range-chart-spec.js
@@ -37,7 +37,7 @@ const chartSpecSelector = createKeyedStateSelector(
       // },
       bounds: "flush",
       config: {
-        background: "#f9f9fa",
+        background: "transparent",
         axis: {
           labelFont: fontFamily,
           labelFontSize: 11,
