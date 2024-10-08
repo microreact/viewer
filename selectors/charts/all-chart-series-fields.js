46b6748737eb5e524f9b1ff33dfae7f5e4132455
khalil-abudahab <khalil.abudahab@cgps.group>
2024-10-08 17:32:33 +0100

    Add pie chart colours to legend.

--- selectors/charts/all-chart-series-fields.js
+++ selectors/charts/all-chart-series-fields.js
@@ -5,7 +5,10 @@ import chartStateSelector from "./chart-state";
 
 const allChartsSeriesFieldsSelector = createCombinedStateSelector(
   (state) => state.charts,
-  (state, chartId) => chartStateSelector(state, chartId).seriesField,
+  (state, chartId) => {
+    const chartState = chartStateSelector(state, chartId);
+    return chartState.seriesField ?? chartState.categoriesField;
+  },
   (fields) => {
     return Array.from(new Set(fields || emptyArray));
   },
