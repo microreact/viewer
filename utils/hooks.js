28684c0fbe8f7a1bf28e771d21562d46441e9dbf
khalil-abudahab <khalil.abudahab@cgps.group>
2024-10-12 07:25:33 +0100

    Update hooks.js

--- utils/hooks.js
+++ utils/hooks.js
@@ -6,10 +6,10 @@ export const useAppDispatch = useDispatch;
 
 export const useAppSelector = useSelector;
 
-export const usePresentSelector = (selector, ...args) => {
+export const usePresentSelector = (baseSelector, ...args) => {
   return (
     useSelector(
-      (rootState) => selector(presentStateSelector(rootState), ...args)
+      (rootState) => baseSelector(presentStateSelector(rootState), ...args)
     )
   );
 };
