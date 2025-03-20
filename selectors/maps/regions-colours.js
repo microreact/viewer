b7ca96866b0bc8f5422d7d57e3112fb17c6398a0
Khalil <khalil.abudahab@cgps.group>
2025-03-20 16:31:44 +0000

    Update regions-colours.js

--- selectors/maps/regions-colours.js
+++ selectors/maps/regions-colours.js
@@ -156,18 +156,18 @@ const regionColoursSelector = createKeyedStateSelector(
       let domain;
       let colourGetter;
       if (regionsColourScale === "binned") {
-        const colorRange = colourPalette.entries;
-        // [
-        //   "#f7fcfd",
-        //   "#e0ecf4",
-        //   "#bfd3e6",
-        //   "#9ebcda",
-        //   "#8c96c6",
-        //   "#8c6bb1",
-        //   "#88419d",
-        //   "#810f7c",
-        //   "#4d004b",
-        // ];
+        // const colorRange = colourPalette.entries;
+        const colorRange = [
+          "#f7fcfd",
+          "#e0ecf4",
+          "#bfd3e6",
+          "#9ebcda",
+          "#8c96c6",
+          "#8c6bb1",
+          "#88419d",
+          "#810f7c",
+          "#4d004b",
+        ];
         // const colorRange = [
         //   "grey",
         //   "grey",
